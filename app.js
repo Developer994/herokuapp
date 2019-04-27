002d6d89abd4b3f4ae599638d89b5fcaab2da131
Silent Dev <rez_a@outlook.com>
2019-04-27 12:58:43 -0400

    Changes

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
+// Dependencies
 var express = require("express");
+var path = require("path");
 
+// Set up the express app
+var app = express();
+var PORT = process.env.PORT || 3000;
 
+//Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+app.get("/", function (req, res) {
+    res.send("Welcome to the Star Wars Page!");
+});
+
+
+// Listener
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
+
+
+app.get("/", function (req, res) {
+    res.json(path.join(__dirname, "public/index.html"));
+});
